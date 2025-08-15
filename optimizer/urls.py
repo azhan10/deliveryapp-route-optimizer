b22a24550fff4562d563e18656f3715e07e86f14
Azhan Rashid <azhanrashid@Azhans-MacBook-Pro-6.local>
2025-08-15 08:17:56 +0100

    feat(test): made the url more flexible

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -1,14 +1,12 @@
 
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 urlpatterns = [
-    path('optimize-route/', views.optimize_route_view, name='optimize_route_view'),
-    path('data-source/', views.data_source_table_view, name='data_source_table_view'),
+	re_path(r'optimize-route/?$', views.optimize_route_view, name='optimize_route_view'),
+	re_path(r'data-source/?$', views.data_source_table_view, name='data_source_table_view'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
